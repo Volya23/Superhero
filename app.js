bb879bf4e1929629fdf1b1e46e29fbf516034cc3
Volya23 <v.o.kryzhanivska@gmail.com>
2023-12-16 14:03:05 +0200

    server+controller

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require ('expess');
+
+const app = express();
+
+module.exports = app;
---
+++ controllers/hero.controller.js
@@ -0,0 +1,19 @@
+module.exports.createHero = async (req, res, next) => {
+
+}
+
+module.exports.getOneHero = async (req, res, next) => {
+
+}
+
+module.exports.getAllHeroes = async (req, res, next) => {
+
+}
+
+module.exports.updateHero = async (req, res, next) => {
+
+}
+
+module.exports.deleteHero = async (req, res, next) => {
+
+}
